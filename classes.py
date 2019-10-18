6c10c50d384bd602dab4d704796c8577d15e276a
Poo Gamer <poo@gamer.nom>
2019-10-18 12:49:31 +1100

    added some docstrings to classes

--- classes.py
+++ classes.py
@@ -5,7 +5,25 @@ import requests
 import os
 
 class TwilioClient:
+    """Class for making HTTP requests to Twilio REST API
+    
+    This class is for managing the communication between
+    the Flask app and the Twilio API.
+    
+    Attributes:
+        account_sid: SID specific to the account being used,
+        also retrieved from environment.
+        auth_token: Private token passcode scooped out of
+        environment variable.
+        URL: Twilio main public endpoint for any account.
+        URI: URL + account_sid and '/Messages.json' to make
+        the authenticated endpoint.
+
+    Methods:
+        post_message:
+    """
     def __init__(self, account_sid=None, auth_token=None):
+        """Inits TwilioClient class with params."""
         self.account_sid = account_sid
         self.auth_token = auth_token
         self.URL = 'https://api.twilio.com/2010-04-01/Accounts/'
@@ -13,19 +31,33 @@ class TwilioClient:
         print(self.URI)
         print(self.account_sid)
 
-    def post_message(self, _whatsapp, _body, _number):
+    def post_message(self, whatsapp, body, number):
+        """Method for POST requests to Twilio endpoint
+
+        Args:
+            whatsapp: Boolean indicating whether or not use WhatsApp - this
+                class can be used for SMS - simply make call this method
+                with False as the first argument.
+            body: String of the body text
+            number: String E167 number formatted internation phone number
+
+        Returns:
+            response: The object returned by Twilio REST API is a
+            JSON to dictionary object.
+            
+        """
         message_data = {
-                'Body': _body,
+                'Body': body,
                 'From': 'whatsapp:+14155238886',
-                'To': f'whatsapp:{_number}'}
-        if _whatsapp == False:
+                'To': f'whatsapp:{number}'}
+        if whatsapp == False:
             message_data['From'] = '+12162796757'
-            message_data['To'] = _number
+            message_data['To'] = number
         response = requests.post(
                 self.URI,
                 data=message_data,
                 auth=(self.account_sid, self.auth_token))
-        print(response)
+        return response
 
 
 #account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -42,8 +74,8 @@ def check_get_response(get_response_object):
         print('Fail, dude')
 
 
-response = requests.get('https://api.twilio.com')
-check_get_response(response)
+#response = requests.get('https://api.twilio.com')
+#check_get_response(response)
 
 
 #connection = http.client.HTTPSConnection('api.twilio.com')
